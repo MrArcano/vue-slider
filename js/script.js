a615659fc2b8896153732233fa1eeba32661af43
MrArcano <salvatore-dario@hotmail.it>
2023-09-28 16:04:48 +0200

    [edit]: style

--- js/script.js
+++ js/script.js
@@ -5,23 +5,23 @@ createApp({
     return {
       elements: [
         {
-          src: 'img/01.webp',
+          src: 'img/01.jpg',
           title: 'Marvel\'s Spiderman Miles Morale',
           text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
       }, {
-          src: 'img/02.webp',
+          src: 'img/02.jpg',
           title: 'Ratchet & Clank: Rift Apart',
           text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
       }, {
-          src: 'img/03.webp',
+          src: 'img/03.jpg',
           title: 'Fortnite',
           text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
       }, {
-          src: 'img/04.webp',
+          src: 'img/04.jpg',
           title: 'Stray',
           text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
       }, {
-          src: 'img/05.webp',
+          src: 'img/05.jpg',
           title: "Marvel's Avengers",
           text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
       }
